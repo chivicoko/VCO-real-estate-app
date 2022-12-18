7285b4d2dacea0cd0ee411610f60537b37ad28ba
Victor <vicharmonic@gmail.com>
2022-12-18 21:43:13 +0000

    Tiding upp

--- realtors/admin.py
+++ realtors/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Realtor
 
-# Register your models here.
 
 class RealtorAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'email', 'hire_date')
