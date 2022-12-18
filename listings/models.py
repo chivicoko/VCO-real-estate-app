bb5a42579fc4181252870694ba1ce2c3d2c62551
Victor <vicharmonic@gmail.com>
2022-12-18 21:40:02 +0000

    Tiding up

--- listings/models.py
+++ listings/models.py
@@ -3,7 +3,6 @@ from django.db.models.fields import NOT_PROVIDED
 from datetime import datetime
 from realtors.models import Realtor
 
-# Create your models here.
 
 class Listing(models.Model):
     realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
@@ -28,6 +27,7 @@ class Listing(models.Model):
     photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     is_published = models.BooleanField(default=True)
     list_date = models.DateTimeField(default=datetime.now, blank=True)
+
     def __str__(self):
         return self.title
 
