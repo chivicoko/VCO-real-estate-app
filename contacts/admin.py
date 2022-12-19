a1b2933220a83a85399c658128cad5be62f1de47
Victor <vicharmonic@gmail.com>
2022-12-19 05:17:09 +0000

    tiding up contacts admin

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Contact
 
-# Register your models here.
 
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'listing', 'email', 'contact_date')
