3d9157b17979a8951f215cbf8df10e8f848226be
Victor <vicharmonic@gmail.com>
2022-12-19 05:17:53 +0000

    tiding up contacts models

--- contacts/models.py
+++ contacts/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from datetime import datetime
 
-# Create your models here.
 
 class Contact(models.Model):
     listing = models.CharField(max_length=200)
@@ -12,5 +11,6 @@ class Contact(models.Model):
     message = models.TextField(blank=True)
     contact_date = models.DateTimeField(default=datetime.now, blank=True)
     user_id = models.IntegerField(blank=True)
+   
     def __str__(self):
         return self.name
