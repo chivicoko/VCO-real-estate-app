8a9c9f994b5615b01ddcb921f883cf25fb6a94e7
Victor <vicharmonic@gmail.com>
2022-12-19 05:11:54 +0000

    Tiding up models

--- realtors/models.py
+++ realtors/models.py
@@ -1,8 +1,6 @@
 from datetime import datetime
 from django.db import models
 
-# Create your models here.
-
 class Realtor(models.Model):
     name = models.CharField(max_length=200)
     photo = models.ImageField(upload_to='Photos/%Y/%m/%d/')
